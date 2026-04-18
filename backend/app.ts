35693e47dfcf11321f4146d1a3dac6fca1ec3e50
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-04-18 12:06:30 -0400

    working on the backend

--- backend/app.ts
+++ backend/app.ts
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.json());   
 
 //import the routes here as such 
- 
+import getPuppeteerRoute from './routes/getpuppeteer.route';  
+
+//Instantiate routes here as such
+app.use('/api/v1/', getPuppeteerRoute); 
 
 app.get('/', (req: Request, res: Response) => { 
     res.send('Server Successfully Running'); 

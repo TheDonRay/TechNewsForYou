0d4f0657b18ba5e7ae69322881a5c047cb9595b6
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-04-24 20:56:12 -0400

    working on the second site scrape will test tommorow

--- backend/services/scraper-site2.service.ts
+++ backend/services/scraper-site2.service.ts
@@ -11,20 +11,39 @@ const secondsitescrape = async () : Promise<any> =>  {
         await page.goto('https://techcrunch.com/');  
 
         // scroll down  
-        const scrolltoarticle = await page.$('loop-card__title');  
+        const scrolltoarticle = await page.$('.loop-card__title');  
         if  (scrolltoarticle) { 
             await scrolltoarticle.evaluate(el => el.scrollIntoView({ behavior: 'smooth', block: 'center'})); 
             console.log('Successfully scrolled into view'); 
         } else { 
             console.log('element not found'); 
-        }
+        } 
+
+        //get title real quick 
+        const titleofPage = await page.title(); 
 
         //click on the specific content 
         await Promise.all([ 
             page.waitForNavigation(), 
-            page.click('')
-        ])
-    } catch (error) { 
+            page.click('a.loop-card__title-link'),
+        ]);  
+        console.log('successfully clicked link'); 
 
+        //get article title 
+        const articleTitle = await page.title(); 
+        // now retrieve the text here as such 
+        const sitetext = await page.$$eval(
+            '.entry-content p.wp-block-paragraph',
+            elements => elements.map(el => el.innerText).join('\n')
+        );
+        console.log(sitetext); 
+
+        await browser.close(); 
+
+        // return object data here 
+        return {titleofPage, articleTitle, sitetext}; 
+
+    } catch (error) { 
+        console.error('There was error visiting site', error); 
     }
 }

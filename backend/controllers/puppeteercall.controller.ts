3e16af8b46cf9b5d0c79036094a051e30b985831
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-01 12:56:36 -0400

    pushing changes i forgot to push

--- backend/controllers/puppeteercall.controller.ts
+++ backend/controllers/puppeteercall.controller.ts
@@ -3,14 +3,14 @@
 import { Request, Response } from "express";
 // call the services folder to get the puppeteer function
 import webscrapesiteOne from "../services/scraper-site1.service.js";
-import secondsitescript from "../services/scraper-site2.service.js";
+// import secondsitescript from "../services/scraper-site2.service.js";
 
 const puppeteerControllerCall = async (req: Request, res: Response) => {
   // set up try and catch case
   try {
     // TODO: change this route to return json object data instead.
     // invoke the function here as such
-    const retrieveScrapedData = await secondsitescript();
+    const retrieveScrapedData = await webscrapesiteOne();
     // handle some error handling here
     if (!retrieveScrapedData) {
       throw new Error("Error could not get data from the site");
